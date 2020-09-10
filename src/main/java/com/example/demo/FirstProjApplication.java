519500733de357a068ba5e94bf848f9fb68e88f0
Shrey Pithava <shreypithava@Shreys-MacBook-Pro.local>
2020-09-10 10:05:29 -0500

    added Alien class

--- src/main/java/com/example/demo/FirstProjApplication.java
+++ src/main/java/com/example/demo/FirstProjApplication.java
@@ -2,12 +2,15 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class FirstProjApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FirstProjApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(FirstProjApplication.class, args);
+		Alien a = context.getBean(Alien.class);
+		a.show();
 	}
 
 }
